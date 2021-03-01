c531abf94da2d3f56b169cb88ce2db4d23dd4eca
rks-69 <rishabhks1998@gmail.com>
2021-03-01 09:26:12 +0000

    Arithmetic Calculator First Commit

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double num1,num2;
+        System.out.println("Enter first number:");
+        num1=sc.nextDouble();
+        System.out.println("Enter second number:");
+        num2=sc.nextDouble();
+        System.out.println("Enter an operator: +, -, *, /");
+        char ch=sc.next().charAt(0);
+        double output;
+        switch(ch)
+        {
+            case '+':
+                output=num1+num2;
+                break;
+            case '-':
+                output=num1-num2;
+                break;
+            case '*':
+                output=num1*num2;
+                break;
+            case '/':
+                output=num1/num2;
+                break;
+            default:
+                System.out.println("Wrong operator encountered!!!");
+                return;
+        }
+        System.out.println(num1+" "+ch+" "+num2+" "+output);
+    }
+}
